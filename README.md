a524e327e0879e2fbc266939a5adc6f752fdec6f
SM Sohag <sohagsheik32@gmail.com>
2023-04-14 15:05:45 +0600

    google popup sing done

--- README.md
+++ README.md
@@ -9,6 +9,7 @@
 - add config file to your project.
 - Danger: do not publish to public firebase config.
 - Router setup.
+
 - Visit: to go to Docs> build > Authentication > Web > Get Started
 - export app from the firebase.config.js file: then export default app
 
@@ -16,4 +17,14 @@
 
 - Login.jsx import getAuth from firebase/auth
 - create const auth = getAuth()
+
 - import googleAuthProvider and create a new provider.
+- use singInWithPopUp and pass auth and provider.
+- Activate sing-in method (google, facebook).
+- vite: change 127.0.0.1 to localhost
+- using singOut and pass only auth (then, catch)
+
+### more auth provider
+
+- activate the auth provider (create app, provide redirect url, client id, secret id,)
+-
--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,15 +1,53 @@
-import React from "react";
-import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import React, { useState } from "react";
+import {
+  GoogleAuthProvider,
+  getAuth,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 import app from "../Firebase/firebase.init";
 const Login = () => {
+  const [user, setUser] = useState(null);
   const auth = getAuth(app);
 
   const provider = new GoogleAuthProvider();
 
-  const handleGoogleSignIn = () => {};
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then((result) => {
+        const loggedInUser = result.user;
+        setUser(loggedInUser);
+        console.log(loggedInUser);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleSingOut = () => {
+    signOut(auth)
+      .then((result) => {
+        console.log(result);
+        setUser(null);
+      })
+      .catch((error) => console.log(error));
+  };
   return (
     <div>
-      <button onClick={handleGoogleSignIn}>Google Login</button>
+      {user ? (
+        <button onClick={handleSingOut}>Sing Out</button>
+      ) : (
+        <button onClick={handleGoogleSignIn}>Google Login</button>
+      )}
+
+      <br />
+      {user && (
+        <div>
+          <h1>Welcome Mr {user?.displayName}</h1>
+          <h1>User Email : {user?.email}</h1>
+          <p>{user?.displayName}</p>
+          <img src={user?.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 };
